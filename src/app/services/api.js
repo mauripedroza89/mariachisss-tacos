3959f47cf1effb89eb48ed2ec65a0b2f4b8217a6
Mauricio Munoz <mauripedroza89@gmail.com>
2021-09-07 21:46:02 -0500

    netlify deployed

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-const baseURL = isProduction ? 'www.production.com' : "http://localhost:3001/api"
+const baseURL = isProduction ? 'https://mariachissstacos.herokuapp.com/' : "http://localhost:3001/api"
 
 export const _api = axios.create({
     baseURL,
